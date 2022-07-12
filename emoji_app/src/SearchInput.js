46a325fa25f266019a361175bc634b69c80d98f0
Berlin <Berline_lucien@yahoo.com>
2022-07-11 23:10:45 -0400

    downloaded files and created react

---
+++ emoji_app/src/SearchInput.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+import "./SearchInput.css";
+
+
+class SearchInput extends PureComponent {
+    static propTypes = {
+        textChange: PropTypes.func
+    };
+
+    handleChange = event => {
+        this.props.textChange(event);
+    };
+
+    render(){
+        return (
+            <div className="component-search-input">
+                <div>
+                    <input onChange={this.handleChange} />
+                </div>
+            </div>
+
+        )
+    }
+    }
+
+    export default SearchInput;
